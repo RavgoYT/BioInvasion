8d4fbe80767f5f3854615efa62d736e0dc8739ff
RavgoYT <88573982+RavgoYT@users.noreply.github.com>
2023-06-06 16:06:14 -0700

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,19 @@
+let enemi = [
+    'virus',
+    'bacteria',
+    'fungi',
+    'parasite'
+  ]
+
+  let enemies = {
+    "virus": 'viruspng',
+    "bacteria": 'bacteriapng',
+    "fungi": 'fungipng',
+    "parasite": 'parasitepng'
+  }
+
+
+let random = Math.floor(Math.random() * 4);
+
+console.log(random)
+
